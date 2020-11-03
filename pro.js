87ec57efd153bf58a2bc6cf361aa87a2c9cfa7d6
matheuslm02 <matheushuck06@gmail.com>
2020-11-03 10:08:58 -0300

    opa

--- pro.js
+++ pro.js
@@ -39,3 +39,65 @@ window.addEventListener('scroll', debounce(function(){
 }, 100));
 
 }
+
+
+
+
+
+
+
+var listElement = document.querySelector('#app ul');
+var inputElement = document.querySelector('#app input');
+var buttonElement = document.querySelector('#app button');
+
+var pecas = JSON.parse(localStorage.getItem('list_pecas')) || [];
+
+function renderPecas(){
+    listElement.innerHTML = '';
+
+    for(peca of pecas) {
+        var pecaElement = document.createElement('li');
+        var pecaText = document.createTextNode(peca);
+
+        var linkElement = document.createElement('a');
+         
+        linkElement.setAttribute('href', '#');
+
+        var pos = pecas.indexOf(peca);
+        linkElement.setAttribute('onclick', 'deletePeca(' + pos + ')');
+
+        var linkText = document.createTextNode('Excluir');
+
+          linkElement.appendChild(linkText);
+
+          pecaElement.appendChild(pecaText);
+          pecaElement.appendChild(linkElement);
+          listElement.appendChild(pecaElement);
+    }
+}
+
+renderPecas();
+
+function addPecas(){
+    var pecaText = inputElement.value;
+
+    pecas.push(pecaText);
+    inputElement.value = '';
+    renderPecas();
+    savePeStorage();
+}
+
+buttonElement.onclick = addPecas;
+
+function deletePeca(pos){
+  pecas.splice(pos, 1);
+  renderPecas();
+  savePeStorage();
+}
+
+function savePeStorage(){
+  localStorage.setItem('list_pecas', JSON.stringify(pecas));
+}
+
+
+
